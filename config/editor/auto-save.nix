09b076043a816bee37d5451428991e9d7b04420a
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-03-21 01:04:47 +0000

    add autosave to editor module

---
+++ config/editor/auto-save.nix
@@ -0,0 +1,8 @@
+{
+  plugins.auto-save = {
+    enable = true;
+    settings = {
+      debounce_delay = 100;
+    };
+  };
+}
--- config/editor/default.nix
+++ config/editor/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./auto-save.nix
     ./colorscheme.nix
     ./smartyank.nix
     ./treesitter.nix
