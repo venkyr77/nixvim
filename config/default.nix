01bac2dd17f7ffc6a4245a7edcfb6ba99bcfc28a
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-03-21 00:33:09 +0000

    remove bufferline file and global opts

--- config/default.nix
+++ config/default.nix
@@ -1,4 +1,5 @@
 {
-  # Import all your configuration modules here
-  imports = [ ./bufferline.nix ];
+  imports = [
+    ./options.nix
+  ];
 }
---
+++ config/options.nix
@@ -0,0 +1,26 @@
+{
+  globalOpts = {
+    number = true;
+    relativenumber = true;
+    mouse = "a";
+    laststatus = 0;
+    breakindent = true;
+    undofile = true;
+    signcolumn = "yes";
+    updatetime = 250;
+    timeoutlen = 300;
+    inccommand = "split";
+    cursorline = true;
+    scrolloff = 5;
+    ignorecase = true;
+    smartcase = true;
+    splitright = true;
+    splitbelow = true;
+    list = true;
+    listchars.__raw = "{ tab = '» ', trail = '·', nbsp = '␣' }";
+  };
+  globals = {
+    mapleader = " ";
+    maplocalleader = " ";
+  };
+}
