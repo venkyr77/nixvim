28ebc928efb81f102cbf74b6cda73849dee871d9
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-03-24 12:20:27 +0000

    add catppuccin integrations

--- config/editor/colorscheme.nix
+++ config/editor/colorscheme.nix
@@ -1,6 +1,25 @@
 {
   colorschemes.catppuccin = {
-    enable = true;
     autoLoad = true;
+    enable = true;
+    settings.integrations = {
+      lsp_saga = true;
+      noice = true;
+      notify = true;
+      which_key = true;
+    };
+  };
+  plugins = {
+    bufferline.settings.highlights.__raw =
+      # lua
+      ''
+        require("catppuccin.groups.integrations.bufferline").get()
+      '';
+    lspsaga.ui.kind.__raw =
+      # lua
+      ''
+        require("catppuccin.groups.integrations.lsp_saga").custom_kind()
+      '';
+    lualine.settings.options.theme = "catppuccin";
   };
 }
