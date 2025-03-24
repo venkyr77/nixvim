8efe7204dd1b2789c548c270e4be660d15374051
Venkatesan Ravi <venkyrocker7777@gmail.com>
2025-03-24 02:11:01 +0000

    add inlay hint support for jdtls

--- config/lsp/jdtls.nix
+++ config/lsp/jdtls.nix
@@ -13,26 +13,42 @@
   in {
     enable = true;
     cmd = [];
-    extraOptions.cmd = [
-      "${lib.getExe pkgs.jdt-language-server}"
-      "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar"
-      "-configuration"
-      {
-        __raw =
-          # lua
-          ''
-            vim.fn.stdpath("cache") .. "/jdtls/config"
-          '';
-      }
-      "-data"
-      {
-        __raw =
-          # lua
-          ''
-            vim.fn.stdpath("cache") .. "/jdtls/workspace/" .. vim.fn.fnamemodify(${root_dir}, ":p:h:t")
-          '';
-      }
-    ];
+    extraOptions = {
+      cmd = [
+        "${lib.getExe pkgs.jdt-language-server}"
+        "--jvm-arg=-javaagent:${pkgs.lombok}/share/java/lombok.jar"
+        "-configuration"
+        {
+          __raw =
+            # lua
+            ''
+              vim.fn.stdpath("cache") .. "/jdtls/config"
+            '';
+        }
+        "-data"
+        {
+          __raw =
+            # lua
+            ''
+              vim.fn.stdpath("cache") .. "/jdtls/workspace/" .. vim.fn.fnamemodify(${root_dir}, ":p:h:t")
+            '';
+        }
+      ];
+      on_attach.__raw =
+        #lua
+        ''
+          function(_, bufnr)
+            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+          end
+        '';
+    };
     rootDir.__raw = root_dir;
+    settings = {
+      java = {
+        inlayHints = {
+          parameterNames.enabled = "all";
+        };
+      };
+    };
   };
 }
